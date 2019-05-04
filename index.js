88c0970fac2211d26eb3a4060a693b36ad1f9f0f
Phan Văn Tấn <tanphan0805@gmail.com>
2019-05-04 18:42:04 +0700

    update link api

--- index.js
+++ index.js
@@ -16,30 +16,36 @@ const io = socketIO(server);
 var dataAPI = null;
 
 
-fetch('http://bonddemo.tk/v1/question/render-question?difficulty=2',{
+fetch('http://bonddemo.tk/v1/question/request-question-node', {
         method: 'GET',
         headers: {
             'Authorization': 'Bearer lyWyy7-2EqXt6JOjKXnQV90Ghv94ie_5vO20rHFP',
             'Content-Type': 'text/plain'
         },
-})
-.then(res => res.json())
-.then(response => {
-    dataAPI = response;
-    console.log(dataAPI);
-})
-.catch(error => console.log(error));
+    })
+    .then(res => res.json())
+    .then(response => {
+        dataAPI = response;
+        console.log(dataAPI);
+    })
+    .catch(error => console.log(error));
 
 
 // This is what the socket.io syntax is like, we will work this later
 io.on('connection', socket => {
-  console.log('User connected')
-  
-  io.emit('BROADCAST_QUESTION_TO_CLIENT', dataAPI);
+    console.log('User connected')
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
-  })
+    setTimeout(function () {
+        io.emit('BROADCAST_QUESTION_TO_CLIENT', dataAPI);
+        setTimeout(function () {
+            console.log('time out')
+            io.emit('CLOSE_QUESTION');
+        }, 10000)
+    }, 0);
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
 })
 
 server.listen(port, () => console.log(`Listening on port ${port}`))
