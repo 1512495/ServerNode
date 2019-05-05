2ea101935a7d5ecf718b58654b6dda35f8450c78
Phan Văn Tấn <tanphan0805@gmail.com>
2019-05-06 00:01:06 +0700

    broadcast question

--- index.js
+++ index.js
@@ -13,36 +13,35 @@ const server = http.createServer(app);
 // This creates our socket using the instance of the server
 const io = socketIO(server);
 
-var dataAPI = null;
-
-
-fetch('http://bonddemo.tk/v1/question/request-question-node', {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer lyWyy7-2EqXt6JOjKXnQV90Ghv94ie_5vO20rHFP',
-            'Content-Type': 'text/plain'
-        },
-    })
-    .then(res => res.json())
-    .then(response => {
-        dataAPI = response;
-        console.log(dataAPI);
-    })
-    .catch(error => console.log(error));
-
 
 // This is what the socket.io syntax is like, we will work this later
 io.on('connection', socket => {
     console.log('User connected')
 
-    setTimeout(function () {
-        io.emit('BROADCAST_QUESTION_TO_CLIENT', dataAPI);
-        setTimeout(function () {
-            console.log('time out')
-            io.emit('CLOSE_QUESTION');
-        }, 10000)
-    }, 0);
+    socket.on('GO_TO_GET_QUESTION', () => {
+        fetch('http://bonddemo.tk/v1/question/request-question-node', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer lyWyy7-2EqXt6JOjKXnQV90Ghv94ie_5vO20rHFP',
+                'Content-Type': 'text/plain'
+            },
+        })
+        .then(res => res.json())
+        .then(response => {
 
+            setTimeout(function () {
+                io.emit('BROADCAST_QUESTION_TO_CLIENT', response);
+                setTimeout(function () {
+                    console.log('time out')
+                    io.emit('CLOSE_QUESTION');
+                }, 10000)
+            }, 0);
+        
+        })
+        .catch(error => console.log(error));
+    })
+
+    
     socket.on('disconnect', () => {
         console.log('user disconnected')
     })
