15611f070ecf3e2dbe109f1dcf01f198b4af4269
Phan Văn Tấn <tanphan0805@gmail.com>
2019-05-23 23:09:37 +0700

    add socket

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const fetch = require('node-fetch');
 const socketIO = require('socket.io');
+const FormData = require('form-data');
 
 // our localhost port
 const port = 1235;
@@ -14,6 +15,8 @@ const server = http.createServer(app);
 const io = socketIO(server);
 
 const room = "COMMON_ROOM";
+var current_id = "";
+var current_answer = "";
 
 
 // This is what the socket.io syntax is like, we will work this later
@@ -32,7 +35,7 @@ io.on('connection', socket => {
         })
         .then(res => res.json())
         .then(response => {
-
+            console.log(response);
             setTimeout(function () {
                 io.emit('BROADCAST_QUESTION_TO_CLIENT', response);
                 setTimeout(function () {
@@ -47,7 +50,37 @@ io.on('connection', socket => {
 
     socket.on("CLIENT_CHAT", (data) => {
 		io.sockets.in(room).emit("SERVER_CHAT", data);
-	})
+    })
+    
+
+    socket.on("CHECK_ANSWER", (data) => {
+		var form = new FormData();
+        form.append('email', data[0]);
+        form.append('answer', data[1]);
+        form.append('id', data[2]);
+
+        current_id = data[2];
+
+        fetch('http://bonddemo.tk/v1/question/check-answer', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer lyWyy7-2EqXt6JOjKXnQV90Ghv94ie_5vO20rHFP',
+            },
+            body: form
+        })
+        .then(res => {
+            res.json().then(response => {
+                console.log(response);
+                current_answer = response.answer;
+            })
+        })
+    })
+
+
+    socket.on('RESPONSE_ANSWER_TO_NODE', () => {
+        var res_data = [current_id, current_answer];
+        io.emit("RESPONSE_ANSWER_TO_CLIENT", res_data);
+    });
 
     
     socket.on('disconnect', () => {
