ecc29a0f510ab0cf8beae63717fb7872d84f33ab
Phan Văn Tấn <tanphan0805@gmail.com>
2019-05-27 00:20:50 +0700

    new api

--- index.js
+++ index.js
@@ -15,18 +15,31 @@ const server = http.createServer(app);
 const io = socketIO(server);
 
 const room = "COMMON_ROOM";
-var current_id = "";
-var current_answer = "";
+var arrayAnswer = [];
+
+//get array answer of program
+fetch('http://bonddemo.tk/v1/question/program-question', {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer lyWyy7-2EqXt6JOjKXnQV90Ghv94ie_5vO20rHFP',
+            'Content-Type': 'text/plain'
+        },
+    })
+    .then(res => res.json())
+    .then(response => {
+        console.log(response);
+        arrayAnswer = response;
+    })
+    .catch(error => console.log(error));
 
 
-// This is what the socket.io syntax is like, we will work this later
 io.on('connection', socket => {
     console.log('User connected')
 
     socket.join(room);
 
-    socket.on('GO_TO_GET_QUESTION', () => {
-        fetch('http://bonddemo.tk/v1/question/request-question-node', {
+    socket.on('GO_TO_GET_QUESTION', (program_id) => {
+        fetch('http://bonddemo.tk/v1/question/render-question-program?sttQuestion='+program_id, {
             method: 'GET',
             headers: {
                 'Authorization': 'Bearer lyWyy7-2EqXt6JOjKXnQV90Ghv94ie_5vO20rHFP',
@@ -39,12 +52,9 @@ io.on('connection', socket => {
             setTimeout(function () {
                 io.emit('BROADCAST_QUESTION_TO_CLIENT', response);
                 setTimeout(function () {
-                    //TODO
                     io.emit('CLOSE_QUESTION');
                 }, 12000)
-                
             }, 0);
-        
         })
         .catch(error => console.log(error));
     })
@@ -52,46 +62,11 @@ io.on('connection', socket => {
     socket.on("CLIENT_CHAT", (data) => {
 		io.sockets.in(room).emit("SERVER_CHAT", data);
     })
-    var listAnswer = [];
-
-    socket.on("CHECK_ANSWER", (data) => {
-		var answerClient = {
-            email : data[0],
-            answer: data[1],
-            id : data[2],
-        };
-        listAnswer.push(answerClient);
-		// var form = new FormData();
-        // form.append('email', data[0]);
-        // form.append('answer', data[1]);
-        // form.append('id', data[2]);
-        console.log(listAnswer);
-        // current_id = data[2];
-        // fetch('http://bonddemo.tk/v1/question/check-answer', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Authorization': 'Bearer lyWyy7-2EqXt6JOjKXnQV90Ghv94ie_5vO20rHFP',
-        //     },
-        //     body: form
-        // })
-        // .then(res => {
-        //     res.json().then(response => {
-        //         console.log(response);
-        //         current_answer = response.answer;
-        //     })
-        // })
-    })
-
+    
 
     socket.on('RESPONSE_ANSWER_TO_NODE', () => {
-        var res_data = [current_id, current_answer];
-        io.emit("RESPONSE_ANSWER_TO_CLIENT", res_data);
+        io.emit("RESPONSE_ANSWER_TO_CLIENT", arrayAnswer);
     });
-
-
-    socket.on("SUMMARY", (data)=> {
-        console.log(data);
-    })
 
     
     socket.on('disconnect', () => {
